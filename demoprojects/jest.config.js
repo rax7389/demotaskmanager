9bb5d3a83fbe3f6e13787a6431499a253eb734ab
adarsh.meshram <adarsh.meshram@rakuten.com>
2021-10-30 21:40:46 +0530

    added new lib

--- demoprojects/jest.config.js
+++ demoprojects/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/libs/login',
     '<rootDir>/apps/taskmanagerapi',
     '<rootDir>/libs/core',
+    '<rootDir>/libs/after-login',
   ],
 };
---
+++ demoprojects/libs/after-login/README.md
@@ -0,0 +1,7 @@
+# after-login
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test after-login` to execute the unit tests.
